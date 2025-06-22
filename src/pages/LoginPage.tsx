fe2fbecfb83cc3e5b5c23637b9d730fd907adb25
Juan Prieto <jp1739@gmail.com>
2025-06-22 16:27:38 -0500

    feat: install axios to create method POST to register and login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,61 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+import axios, { AxiosError } from "axios";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await axios.post(
+        "https://the-new-burguer-station-back-jp.up.railway.app/auth/login",
+        { email, password }
+      );
+      const token = response.data.access_token;
+      login(token);
+      navigate("/");
+    } catch (err) {
+      const error = err as AxiosError<{ message: string }>;
+      setError(error.response?.data?.message || "Error al iniciar sesión");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Iniciar Sesión</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <button type="submit">Ingresar</button>
+        </div>
+      </form>
+    </div>
+  );
 };
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,3 +1,69 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios, { AxiosError } from "axios";
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    if (password !== confirm) {
+      setError("Las contraseñas no coinciden");
+      return;
+    }
+
+    try {
+      await axios.post(
+        "https://the-new-burguer-station-back-jp.up.railway.app/auth/register",
+        { email, password }
+      );
+      navigate("/login");
+    } catch (err) {
+      const error = err as AxiosError<{ message: string }>;
+      setError(error.response?.data?.message || "Error al iniciar sesión");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Registro de Usuario</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <label>Confirmar Contraseña:</label>
+          <input
+            type="password"
+            required
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value)}
+          />
+        </div>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button type="submit">Registrarme</button>
+      </form>
+    </div>
+  );
 };
