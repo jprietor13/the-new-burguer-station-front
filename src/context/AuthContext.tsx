9d770bd32432c17824e1446b9c2bf7a279da4e63
Juan Prieto <jp1739@gmail.com>
2025-06-22 15:04:11 -0500

    feat: create AuthContext to catch token JWT

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+import { useState, useContext, createContext } from "react";
+import type { AuthContextType } from "../types/types";
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [token, setToken] = useState<string | null>(() => {
+    return localStorage.getItem("token");
+  });
+
+  const login = (newToken: string) => {
+    localStorage.setItem("token", newToken);
+    setToken(newToken);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
+  const isAuthenticated = !!token;
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth se debe usar dentro de AuthProvider");
+  return context;
+};
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export interface AuthContextType {
+    token: string | null,
+    login: (token: string) =>void;
+    logout: () => void:
+    isAuthenticated: boolean
+}
