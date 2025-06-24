69fbd94b772de300afb2a2842e1d18a2e27a3292
Juan Prieto <jp1739@gmail.com>
2025-06-24 17:25:21 -0500

    fix: delete build files

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <HashRouter basename={import.meta.env.VITE_BASENAME || "/"}>
+    <BrowserRouter>
       <App />
       <Toaster position="top-center" reverseOrder={false} />
-    </HashRouter>
+    </BrowserRouter>
   </AuthProvider>
 );
