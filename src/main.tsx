3b820d79c2f8e4cfd8857eef54070ea1649d0d30
Juan Prieto <jp1739@gmail.com>
2025-06-24 17:10:49 -0500

    build: revert commits to build production

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <BrowserRouter>
+    <HashRouter basename={import.meta.env.VITE_BASENAME || "/"}>
       <App />
       <Toaster position="top-center" reverseOrder={false} />
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>
 );
